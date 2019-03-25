e96c3999744978395b744919d32d941a52e327ba
asadokhin <asadokhin@borlas.ru>
2019-03-25 17:59:36 +0300

    Добавлена сущность Publisher. Изменен соот-ий атрибут в Book

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepostiory;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private AuthorRepostiory authorRepostiory;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepostiory authorRepostiory, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepostiory authorRepostiory, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepostiory = authorRepostiory;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -24,10 +28,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+        publisherRepository.save(publisher);
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        Book ddd = new Book("Domain Driven Design","1234",publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
         authorRepostiory.save(eric);
@@ -35,7 +42,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noeEJB = new Book("J2EE Development without EJB","234444","Work");
+        Book noeEJB = new Book("J2EE Development without EJB","234444",publisher);
         rod.getBooks().add(noeEJB);
 
         authorRepostiory.save(rod);
